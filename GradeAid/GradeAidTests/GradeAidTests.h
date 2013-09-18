1b3af01ec10d95e6009c7f44338d511e9f593aa0
Aerows <daniel.hallin@lunicore.se>
2013-09-18 16:26:26 +0200

    Creation of workspace

---
+++ GradeAid/GradeAidTests/GradeAidTests.h
@@ -0,0 +1,13 @@
+//
+//  GradeAidTests.h
+//  GradeAidTests
+//
+//  Created by Daniel Hallin on 2013-09-18.
+//  Copyright (c) 2013 Daniel Hallin. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface GradeAidTests : SenTestCase
+
+@end
---
+++ GradeAid/GradeAidTests/GradeAidTests.m
@@ -0,0 +1,32 @@
+//
+//  GradeAidTests.m
+//  GradeAidTests
+//
+//  Created by Daniel Hallin on 2013-09-18.
+//  Copyright (c) 2013 Daniel Hallin. All rights reserved.
+//
+
+#import "GradeAidTests.h"
+
+@implementation GradeAidTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in GradeAidTests");
+}
+
+@end
